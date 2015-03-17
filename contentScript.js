695d9921aa65eeb7e500ee1b9c6b181d2bf27fc3
Jorge Rubio <jorgerc85@gmail.com>
2015-03-17 15:34:07 -0400

    ContentScript: Add eventListener for tab close event

--- contentScript.js
+++ contentScript.js
@@ -4,6 +4,10 @@ window.onload = function(event) {
   document.addEventListener("visibilitychange", function(event) {
     sendMessage("running", document.visibilityState, event.timeStamp);
   });
+
+  window.addEventListener("beforeunload", function (event) {
+    sendMessage("closing", document.visibilityState, event.timeStamp);
+  });
 };
 
 function sendMessage(tabState, tabVisibility, time) {
