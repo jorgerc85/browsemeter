023d401e3e946e06d821b4046810a4bdb1c5bd0a
Jorge Rubio <jorgerc85@gmail.com>
2015-03-18 17:07:22 -0400

    Popup.js: Implement basic saving

--- popup.js
+++ popup.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
   websiteOptions = document.getElementsByClassName('websiteOptions')
   for (var i = 0; i < websiteOptions.length; i++) {
-    websiteOptions[i].addEventListener('change', function() {
-      console.log('changed!')
+    websiteOptions[i].addEventListener('change', function(event) {
+      chrome.storage.local.get(function(response) {
+        response[event.target.name] = {
+          'tracking': event.target.checked,
+          'totalTime': 0
+        };
+        chrome.storage.local.set(response, function(response) {
+          console.log("saved")
+        });
+      });
     })
   };
 });
