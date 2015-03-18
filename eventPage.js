d3f83f6e4a287b2b9835cf627e772f7c15ae05c9
Jorge Rubio <jorgerc85@gmail.com>
2015-03-18 12:08:47 -0400

    EventPage.js: Add saving and restoring feature

--- eventPage.js
+++ eventPage.js
@@ -1,3 +1,27 @@
 chrome.runtime.onMessage.addListener(function(message, sender) {
   website = sender.url.match(/\/{2}\w+.(\w+.{1}\w+)\//)[1]
+  switch (message.tabState) {
+    case "opening":
+      savingInfo = { 'openTime': message.time }
+      chrome.storage.local.get(function(info) {
+        info[website] = savingInfo
+        chrome.storage.local.set(info, function(info) {
+          console.log("saved")
+        });
+      });
+      break;
+    case "running":
+      switch (message.tabVisibility) {
+        case "visible":
+          break;
+        case "hidden":
+          break;
+      }
+      break;
+    case "closing":
+      chrome.storage.local.get(function(info) {
+        console.log(info)
+      });
+      break;
+  }
 });
