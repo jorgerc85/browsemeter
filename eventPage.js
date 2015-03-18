61770f2289c801fda9a83f28b1c5c7bbaca315cd
Jorge Rubio <jorgerc85@gmail.com>
2015-03-18 13:43:34 -0400

    EventScript.js: Reformat 'openTime' assignment statement

--- eventPage.js
+++ eventPage.js
@@ -2,9 +2,8 @@ chrome.runtime.onMessage.addListener(function(message, sender) {
   website = sender.url.match(/\/{2}\w+.(\w+.{1}\w+)\//)[1]
   switch (message.tabState) {
     case "opening":
-      savingInfo = { 'openTime': message.time }
       chrome.storage.local.get(function(info) {
-        info[website] = savingInfo
+        info[website]['openTime'] = message.time
         chrome.storage.local.set(info, function(info) {
           console.log("saved")
         });
