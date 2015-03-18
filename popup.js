47116c0da380f2f53f9fa1bb4f6faca4e99c558d
Jorge Rubio <jorgerc85@gmail.com>
2015-03-18 16:01:37 -0400

    Popup.js: Add event listeners to track changes

--- popup.js
+++ popup.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-
+  websiteOptions = document.getElementsByClassName('websiteOptions')
+  for (var i = 0; i < websiteOptions.length; i++) {
+    websiteOptions[i].addEventListener('change', function() {
+      console.log('changed!')
+    })
+  };
 });
