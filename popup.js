4359b852dec481d1f1aceaa537d8b842495eadc7
Jorge Rubio <jorgerc85@gmail.com>
2015-03-24 23:16:13 -0400

    Popup.html/js: Enable counterDisplay

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var websiteOptions = document.getElementsByClassName('websiteOptions')
+  counterDisplay();
+  var websiteOptions = document.getElementsByClassName('websiteOptions');
   for (var i = 0; i < websiteOptions.length; i++) {
     websiteOptions[i].addEventListener('change', function(event) {
       chrome.storage.local.get(function(response) {
@@ -8,9 +9,26 @@ document.addEventListener('DOMContentLoaded', function() {
           'totalTime': 0
         };
         chrome.storage.local.set(response, function(response) {
-          console.log("saved")
+          console.log("saved");
         });
       });
-    })
+    });
   };
 });
+
+function counterDisplay() {
+  chrome.storage.local.get(function(response) {
+    var websiteCounters = document.getElementsByClassName('websiteCounters');
+    for (var i = 0; i < websiteCounters.length; i++) {
+      var counter = websiteCounters[i].getAttribute('name');
+      var totalTime = response[counter]['totalTime'];
+      websiteCounters[i].innerText = timeFormat(totalTime);
+    };
+  });
+};
+
+function timeFormat(totalTime) {
+  minutes = Math.floor(totalTime / 60);
+  seconds = Math.floor(((totalTime / 60) - minutes) * 60);
+  return minutes + ":" + seconds;
+};
